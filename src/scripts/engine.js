330fa0708a61de1eb0b95c5f472eca0b60dc71d3
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-23 17:43:41 -0300

    add pathImages and cardData

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -23,9 +23,41 @@ const state = {
         button: document.getElementById('next-duel'),
     },
 
-
-
 }
+
+const pathImages = './src/assets/icons/';
+
+//enumerando, dando sentido para as cartas
+const cardData = [
+
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}/dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}/magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Sicssors",
+        img: `${pathImages}/exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+
+];
 
 function init() {
 
