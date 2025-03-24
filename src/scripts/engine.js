f5d87ad26ebdb7e6da338adcf10f1bb07067d544
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-24 19:48:30 -0300

    change classes name and img path

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -26,8 +26,8 @@ const state = {
 }
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
 
 const pathImages = './src/assets/icons/';
@@ -79,7 +79,7 @@ async function createCardImage(IdCard, fieldSide) {
     const cardImage = document.createElement('img');
 
     cardImage.setAttribute('height', '100px');
-    cardImage.setAttribute('src', '.src/assets/icons/card-back.png');
+    cardImage.setAttribute('src', './src/assets/icons/card-back.png');
     cardImage.setAttribute('data-id', IdCard);
     cardImage.classList.add('card');
 
