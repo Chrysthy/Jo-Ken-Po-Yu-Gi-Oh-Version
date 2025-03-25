be9825b79b637a694d13276bf00249dd5141a436
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-25 20:51:26 -0300

    converting audio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -269,7 +269,7 @@ async function resetDuel() {
 
 async function playAudio(status) {
 
-    const audio = new Audio(`./src/assets/audios/${status}.mp3`);
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
     audio.volume = 0.2;
     audio.play();
 }
